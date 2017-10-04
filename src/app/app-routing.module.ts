76f4074aaccf5162885b33c624da72a3c29f72a0
Jorge Montellanos Díaz <jorgemontellanos@gmail.com>
2017-10-04 11:08:01 -0400

    para que tengan el codigo de avance

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+import { AdminGuard } from './series/guards/admin.guard';
+import { EditorGuard } from './series/guards/editor.guard';
+import { NotfoundComponent } from './content/notfound/notfound.component';
+import { AddUserComponent } from './content/users/add-user/add-user.component';
+import { UsersComponent } from './content/users/users.component';
+import { AuthGuard } from './series/guards/auth.guard';
+import { CalendarComponent } from './content/calendar/calendar.component';
+import { DashboardComponent } from './content/dashboard/dashboard.component';
+import { LoginComponent } from './content/login/login.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component : LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate : [AuthGuard]
+  },
+  {
+    path : 'calendar',
+    component: CalendarComponent,
+    canActivate : [AuthGuard]
+  },
+  {
+    path: 'users',
+    component : UsersComponent,
+    canActivate : [AuthGuard]
+  },
+  {
+    path : 'users/add',
+    component : AddUserComponent,
+    canActivate : [AuthGuard]
+  },
+  {
+    path : '**',
+    component: NotfoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
